d3507c1e682e4917f36c467ab25101b0cdc97caf
Sujit Karki <swoozeki>
2023-07-23 14:47:23 -0400

    init home page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,3 @@
-<div class="p-4">
-    <h1 class="text-3xl">Welcome to SvelteKit</h1>
-    <p>
-        Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation
-    </p>
+<div class="p-4 h-screen flex items-center justify-center">
+    <h1 class="text-3xl">Swoozeki</h1>
 </div>
